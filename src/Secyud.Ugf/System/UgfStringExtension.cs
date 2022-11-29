586d2411848ef0fcff638ebfa8cfe706f94416fe
Secyud <2427796523@qq.com>
2022-11-29 15:08:59 +0800

    :sparkles: add string extension

---
+++ src/Secyud.Ugf/System/UgfStringExtension.cs
@@ -0,0 +1,15 @@
+namespace System
+{
+    public static class UgfStringExtension
+    {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+        
+        public static bool IsNullOrWhiteSpace(this string s)
+        {
+            return string.IsNullOrWhiteSpace(s);
+        }
+    }
+}
