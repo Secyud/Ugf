fbb7caf5713722a71a2f4fc86c747cc50da6ec33
老土 <69134017+Secyud@users.noreply.github.com>
2023-11-08 09:50:32 +0800

    add limit child option

--- UgfBase/Secyud/Ugf/DataManager/TypeLimitAttribute.cs
+++ UgfBase/Secyud/Ugf/DataManager/TypeLimitAttribute.cs
@@ -5,11 +5,14 @@ namespace Secyud.Ugf.DataManager
     [AttributeUsage(AttributeTargets.Field)]
     public class TypeLimitAttribute:Attribute
     {
-        public TypeLimitAttribute(Type limitType)
+        public TypeLimitAttribute(Type limitType, bool limitChild = false)
         {
             LimitType = limitType;
+            LimitChild = limitChild;
         }
 
         public Type LimitType { get;}
+        
+        public bool LimitChild { get; }
     }
 }
