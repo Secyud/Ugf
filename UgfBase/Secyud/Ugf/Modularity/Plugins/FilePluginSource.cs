8ff484788ec30cf2b09ec357f3c5d8579f0f4f06
老土 <2427796523@qq.com>
2023-11-20 11:37:50 +0800

    add file plugin source

---
+++ UgfBase/Secyud/Ugf/Modularity/Plugins/FilePluginSource.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Secyud.Ugf.Modularity.Plugins
+{
+    public class FilePluginSource : TypePlugInSource
+    {
+        protected FilePluginSource(List<string> filePaths)
+        {
+            ModuleTypes = filePaths
+                .Select(Assembly.LoadFile)
+                .Select(assembly => assembly.ExportedTypes
+                    .First(u => typeof(IUgfModule).IsAssignableFrom(u)))
+                .Where(type => type is not null).ToArray();
+        }
+    }
+}
