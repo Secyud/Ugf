243e1ead450ecec1d7a5622fa0c43565e1565ec4
老土 <2427796523@qq.com>
2023-12-28 13:50:37 +0800

    add some annotation

--- Ugf.Unity/Secyud/Ugf/EditorComponents/EditorBase.cs
+++ Ugf.Unity/Secyud/Ugf/EditorComponents/EditorBase.cs
@@ -2,26 +2,32 @@
 
 namespace Secyud.Ugf.EditorComponents
 {
-    public abstract class EditorBase<TProperty> :MonoBehaviour
+    /// <summary>
+    /// Editor 的本质是为了设置属性，所以需要绑定一个上下文并设置初值和空值
+    /// </summary>
+    public abstract class EditorBase<TProperty> : MonoBehaviour
     {
         public TProperty Property { get; private set; }
-        
+
         public virtual void Bind(TProperty property)
         {
             Property = property;
             if (property is null)
-                ClearUi();
+            {
+                ClearData();
+            }
             else
+            {
                 InitData();
+            }
         }
 
         protected virtual void InitData()
         {
-            
         }
-        protected virtual void ClearUi()
+
+        protected virtual void ClearData()
         {
-            
         }
     }
 }
--- Ugf.Unity/Secyud/Ugf/EditorComponents/EditorEvent.cs
+++ Ugf.Unity/Secyud/Ugf/EditorComponents/EditorEvent.cs
@@ -4,6 +4,12 @@ using UnityEngine.Events;
 
 namespace Secyud.Ugf.EditorComponents
 {
+    /// <summary>
+    /// 属性设定时应当有所反应， 以确认属性设定的真实值，
+    /// 真实值可能用于文字显示，图形变化，一般加入
+    /// SetWithoutNotification之类的事件
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
     [Serializable]
     public class EditorEvent<TValue>
     {
