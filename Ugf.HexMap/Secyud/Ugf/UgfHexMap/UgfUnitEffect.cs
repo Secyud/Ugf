7715a6b9634c7a80005314761eb83a91aef44b34
老土 <2427796523@qq.com>
2024-01-02 10:49:03 +0800

    remove particle system limit

--- Ugf.HexMap/Secyud/Ugf/UgfHexMap/UgfUnitEffect.cs
+++ Ugf.HexMap/Secyud/Ugf/UgfHexMap/UgfUnitEffect.cs
@@ -2,14 +2,12 @@
 
 namespace Secyud.Ugf.UgfHexMap
 {
-    [RequireComponent(typeof(ParticleSystem))]
     public class UgfUnitEffect : MonoBehaviour
     {
         private UgfUnitEffectDelegate _effectDelegate;
 
         public UgfUnit Unit { get; private set; }
         public UgfCell Target { get; private set; }
-        public ParticleSystem Particle { get; private set; }
 
         public virtual void SetDelegate(UgfUnitEffectDelegate effectDelegate,
             UgfUnit unit, UgfCell target)
@@ -17,11 +15,6 @@ namespace Secyud.Ugf.UgfHexMap
             _effectDelegate = effectDelegate;
             Unit = unit;
             Target = target;
-        }
-
-        protected void Awake()
-        {
-            Particle = GetComponent<ParticleSystem>();
         }
 
         protected virtual void Update()
