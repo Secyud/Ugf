0a41fbe798f920ad7ab26ed78e5ca3fcd5478bd3
老土 <2427796523@qq.com>
2024-01-24 13:04:55 +0800

    add a collapse item

---
+++ Secyud.Ugf.Unity/Secyud/Ugf/Unity/Ui/CollapseItem.cs
@@ -0,0 +1,20 @@
+﻿#region
+
+using UnityEngine;
+
+#endregion
+
+namespace Secyud.Ugf.Unity.Ui
+{
+	public class CollapseItem : MonoBehaviour
+	{
+		[SerializeField] private LayoutTrigger _trigger;
+		[SerializeField] private GameObject _gameObject;
+
+		public void Collapse(bool b)
+		{
+			_gameObject.SetActive(!b);
+			_trigger.Refresh();
+		}
+	}
+}
